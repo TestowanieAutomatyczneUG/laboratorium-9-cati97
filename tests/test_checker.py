82a2e62a13580e4056a0077089d9c0f25ed8f24b
unknown <katasiw97@gmail.com>
2020-12-02 20:36:03 +0100

    test_after_17_played - green

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -14,3 +14,11 @@ class TestChecker(unittest.TestCase):
         self.temp.laptop.wavWasPlayed.return_value = False
         result = self.temp.reset("song.mp3")
         self.assertEqual(result, False)
+
+    def test_after_17_played(self):
+        self.temp.laptop.getTime = Mock(name='getTime')
+        self.temp.laptop.getTime.return_value = 18
+        self.temp.laptop.wavWasPlayed = Mock(name='wavWasPlayed')
+        self.temp.laptop.wavWasPlayed.return_value = True
+        result = self.temp.reset("song.mp3")
+        self.assertEqual(result, True)
