6467e075f03249a94fe0efb3712db65e933ac24b
unknown <katasiw97@gmail.com>
2020-12-02 19:14:35 +0100

    test_needsFuel_return_false - green

--- tests/test_car.py
+++ tests/test_car.py
@@ -11,5 +11,10 @@ class TestCar(unittest.TestCase):
         self.temp.needsFuel = Mock(name='needsFuel')
         self.temp.needsFuel.return_value = True
         self.assertEqual(self.temp.needsFuel(), True)
-    
+
+    def test_needsFuel_return_false(self):
+        self.temp.needsFuel = Mock(name='needsFuel')
+        self.temp.needsFuel.return_value = False
+        self.assertEqual(self.temp.needsFuel(), False)
+        
 
